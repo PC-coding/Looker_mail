3b6272fad8621b7d5ef6d7c171d0f15098b8df3b
PC-coding <partapchauhan8@gmail.com>
2021-05-07 22:52:18 -0400

    sidebar progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header.js';
+// import SideBar from './components/Sidebar.js';
 
 function App() {
   return (
     <div className="App">
       <Header /> 
+      {/* <SideBar /> */}
       <h1>building looker mail</h1>
     </div>
   );
