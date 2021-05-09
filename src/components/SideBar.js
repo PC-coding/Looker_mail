89aeebef6cdb1d7f4d9581cb16f9c6309ddf2caa
PC-coding <partapchauhan8@gmail.com>
2021-05-08 21:11:47 -0400

    working on sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import './SideBar.css';
 
 function SideBar() {
     return (
-        <div>
-            
+        <div className='sideBar'>
+            <Button startIcon={<AddIcon fontSize='large'/>}>Compose</Button>
         </div>
     )
 }
