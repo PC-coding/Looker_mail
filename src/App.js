be04791fde18bd08c4182f06506620018245de6e
PC-coding <partapchauhan8@gmail.com>
2021-05-10 19:25:42 -0400

    rearranged files, style edites

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header.js';
-// import SideBar from './components/Sidebar.js';
+import Header from './components/Header/Header.js';
+import SideBar from './components/Sidebar.js';
 
 function App() {
   return (
     <div className="App">
       <Header /> 
-      {/* <SideBar /> */}
+      <SideBar />
       <h1>building looker mail</h1>
     </div>
   );
