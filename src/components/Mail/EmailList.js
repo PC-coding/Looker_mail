2ce3a6712267857822a6c430417c874940e67f17
PC-coding <partapchauhan8@gmail.com>
2021-05-15 21:13:34 -0400

    working on email list

--- src/components/Mail/EmailList.js
+++ src/components/Mail/EmailList.js
@@ -3,6 +3,9 @@ import { Checkbox, IconButton } from '@material-ui/core';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
 import './EmailList.css';
 
 function EmailList() {
@@ -19,6 +22,20 @@ function EmailList() {
                     </IconButton>
                     <IconButton>
                         <MoreVertIcon />
+                    </IconButton>
+                </div>
+                <div className='emailList_settingsRight'>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
                     </IconButton>
                 </div>
             </div>
