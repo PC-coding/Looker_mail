918e482ea275b6597610a465c0e56edd2d7b1505
PC-coding <partapchauhan8@gmail.com>
2021-05-28 21:14:51 -0400

    added in missing imports

--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -1,6 +1,15 @@
 import { CheckCircle } from '@material-ui/icons';
 import React from 'react';
 import './Mail.css';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
+import ErrorIcon from '@material-ui/icons/Error';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EmailIcon from '@material-ui/icons/Email';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Mail() {
     return (
