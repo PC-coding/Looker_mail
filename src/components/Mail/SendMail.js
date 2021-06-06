5cf0f8853382082292e54f0f10cc69bcead7afcb
PC-coding <partapchauhan8@gmail.com>
2021-06-05 18:06:22 -0700

    working on new component

--- src/components/Mail/SendMail.js
+++ src/components/Mail/SendMail.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './SendMail.css';
+import CloseIcon from '@material-ui/icons/Close';
 
 function SendMail() {
     return (
-        <div>
+        <div className='sendMail'>
+            <div className='sendMail_header'>
+                <p>message</p>
+                <CloseIcon />
+            </div>
             
         </div>
     )
